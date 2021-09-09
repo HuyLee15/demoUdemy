46a8ca2f80ce93d3dad6c515bfc505068539ffb6
Huy <71880560+s2huyo0ohuys2@users.noreply.github.com>
2021-09-09 21:13:40 +0700

    CURD

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,10 @@
+const Joi = require('joi');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const products = [
     { id: 1, name: 'product1' },
@@ -16,9 +20,48 @@ app.get('/api/products', (req, res) => {
     res.send(products);
 })
 
+app.post('/api/products', (req, res) => {
+    if (!req.body.name || req.body.name.length < 3) {
+        //400 bad request
+        res.status(400).send("name is required and should be minimum 3 character.")
+        return;
+    }
+    const product = {
+        id: products.length + 1,
+        name: req.body.name
+    }
+    products.push(product);
+    res.send(product);
+
+})
+
+app.put('/api/products/:id', (req, res) => {
+    const product = products.find(c => c.id === parseInt(req.params.id));
+    if (!product) {
+        res.status(404).send("the product with thi given id was not found")
+    }
+
+
+    product.name = req.body.name;
+    res.send(product);
+});
+
+
+app.delete('/api/products/:id', (req, res) => {
+    const product = products.find(c => c.id === parseInt(req.params.id));
+    if (!product) {
+        return res.status(404).send("the product with thi given id was not found")
+    }
+    const index = products.indexOf(product);
+    products.splice(index, 1);
+    res.send(products)
+})
+
+
+
 app.get('/api/products/:id', (req, res) => {
     const product = products.find(c => c.id === parseInt(req.params.id));
-    if (!product) res.status(404).send("the product with thi given id was not found")
+    if (!product) return res.status(404).send("the product with thi given id was not found")
     res.send(product)
 })
 
