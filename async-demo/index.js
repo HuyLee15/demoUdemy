4640c76274660ba6f6831fb1ca4d1f4b8d46c12e
Huy <71880560+s2huyo0ohuys2@users.noreply.github.com>
2021-09-12 19:21:03 +0700

    async

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,19 +1,31 @@
 console.log('Before');
 
-getUser(1, (user) => {
-    //get the repositories
-    getRepositories(user.gitHubUsername, (repos) => {
-        getCommits(repo[0], (commits) => {
-            console.log(commits);
-        });
-    });
-});
+// getUser(1, (user) => {
+//     //get the repositories
+//     getRepositories(user.gitHubUsername, (repos) => {
+//         getCommits(repo[0], (commits) => {
+//             console.log(commits);
+//         });
+//     });
+// });
 
-const p = getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('Error', err.message));
+// const p = getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err => console.log('Error', err.message));
+
+displayCommits = async() => {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    } catch (err) {
+        console.log('Error', err.message);
+    }
+}
+displayCommits();
 
 console.log('After');
 
@@ -30,7 +42,8 @@ getRepositories = (username) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('Calling GitBub API!');
-            resolve(['repo1', 'repo2', 'repo3']);
+            //resolve(['repo1', 'repo2', 'repo3']);
+            reject(new Error('Could not get the repos'))
         }, 2000);
     });
 };
