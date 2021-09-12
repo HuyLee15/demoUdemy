04c8e1eb2f43444bfe38949cd283cf0659c2898d
Huy <71880560+s2huyo0ohuys2@users.noreply.github.com>
2021-09-12 15:27:37 +0700

    async

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,29 +1,45 @@
 console.log('Before');
-getUser(1, getRepositories);
+
+getUser(1, (user) => {
+    //get the repositories
+    getRepositories(user.gitHubUsername, (repos) => {
+        getCommits(repo[0], (commits) => {
+            console.log(commits);
+        });
+    });
+});
+
+const p = getUser(1)
+    .then(user => getRepositories(user.gitHubUsername))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits', commits))
+    .catch(err => console.log('Error', err.message));
+
 console.log('After');
 
-getRepositories = (user) => {
-    getRepositories(user.gitHubUsername, getCommits);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database!');
+            resolve({ id: id, gitHubUsername: 'HuyLee' });
+        }, 2000);
+    });
 }
+
+getRepositories = (username) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitBub API!');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
+};
 
 getCommits = (repo) => {
-    getCommits(repo, displayCommits);
-}
-
-displayCommits = (commits) => {
-    console.log(commits);
-}
-
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database!');
-        callback({ id: id, gitHubUsername: 'HuyLee' });
-    }, 2000);
-}
-
-getRepositories = (username, callback) => {
-    setTimeout(() => {
-        console.log('Calling GitBub API!');
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
-}
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitBub API!');
+            resolve(['commit']);
+        }, 2000);
+    });
+};
