5e8311f03739659b5ea85a558a3ab72e720131c8
Huy <71880560+s2huyo0ohuys2@users.noreply.github.com>
2021-09-13 16:25:23 +0700

    mongoose

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,10 +4,26 @@ mongoose.connect('mongodb://localhost:27017')
     .then(() => console.log('Connected to MongoDB!!!'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
-const schema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
     name: String,
     author: String,
-    tags: { String },
+    tags: [String],
     date: { type: Date, default: Date.now },
     isPublished: Boolean
 })
+
+const Product = mongoose.model('Product', productSchema);
+
+createProduct = async() => {
+    const product = new Product({
+        name: 'Node.js Product',
+        author: 'Felix',
+        tags: ['reactjs', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await product.save();
+    console.log(result);
+}
+
+createProduct();
