3ea392c1bfa65a7a9efaa1c4640e01ed628343b4
Huy <71880560+s2huyo0ohuys2@users.noreply.github.com>
2021-09-14 21:19:57 +0700

    mongoose

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -5,25 +5,61 @@ mongoose.connect('mongodb://localhost:27017')
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
 const productSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['web', 'mobile', 'network'],
+        lowercase: true,
+        trim: true
+    },
     author: String,
-    tags: [String],
+    tags: {
+        type: Array,
+        validate: {
+            isAsync: true,
+            validator: function(v, callback) {
+                return v && v.length > 0;
+            },
+            message: 'some text'
+        }
+    },
     date: { type: Date, default: Date.now },
-    isPublished: Boolean
-})
+    isPublished: Boolean,
+    price: {
+        type: Number,
+        required: function() { return this.isPublished; },
+        min: 10,
+        max: 200,
+        getter: v => Math.round(v),
+        set: v => Math.round(v)
+    }
+});
 
 const Product = mongoose.model('Product', productSchema);
 
 createProduct = async() => {
     const product = new Product({
         name: 'Node.js Product',
+        category: 'Web',
         author: 'Felix',
-        tags: ['node', 'backend'],
-        isPublished: true
+        tags: ['frontend'],
+        isPublished: true,
+        price: 15.6
     });
+    try {
 
-    const result = await product.save();
-    console.log(result);
+        const result = await product.save();
+        console.log(result);
+    } catch (error) {
+        for (field in error.errors)
+            console.log(error.errors[field].message);
+    }
 }
 
 getProducts = async() => {
@@ -31,15 +67,13 @@ getProducts = async() => {
     const pageNumber = 2;
     const pageSize = 10;
 
-
-
     const products = await Product
-        .find({ author: 'Felix', isPublished: true })
-        .skip((pageNumber - 1) * pageSize)
-        .limit(pageSize)
+        .find({ _id: '6140ab20fd589c965769e179' })
+        // .skip((pageNumber - 1) * pageSize)
+        // .limit(pageSize)
         .sort({ name: 1 })
-        .select({ name: 1, tags: 1 });
-    console.log(products);
+        .select({ name: 1, tags: 1, price: 1 });
+    console.log(products[0].price);
 }
 
 updateProduct = async(id) => {
@@ -60,4 +94,4 @@ removeProduct = async(id) => {
     console.log(product);
 }
 
-removeProduct('613f16ee25d446c86ac7f49f');
+getProducts();
