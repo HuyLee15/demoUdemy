16c8f30bf2f6d1aaaf3f086cf84802094437cb50
Huy <71880560+s2huyo0ohuys2@users.noreply.github.com>
2021-09-20 15:48:29 +0700

    jwt

--- authentication/index.js
+++ authentication/index.js
@@ -7,6 +7,7 @@ const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const express = require('express');
+const auth = require('./routes/auth');
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly')
@@ -19,6 +20,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
